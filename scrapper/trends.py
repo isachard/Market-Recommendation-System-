87114af76c417e0f3c07cd48224ee30992ee0f6e
isachard <isachard@uoregon.edu>
2018-02-06 05:36:36 -0800

    Add files via upload

--- scrapper/trends.py
+++ scrapper/trends.py
@@ -5,13 +5,8 @@ page = 'https://www.covers.com/Sports/nba/PrintSheetHtml?isPrevious=False'
 page = urlopen(page)
 soup = BeautifulSoup(page,'html.parser')
 
-
 topTable = soup.find('tr', attrs={'class':'topbord'}) #701
-
-
 bottomTable = topTable.next_sibling.next_sibling #702
-#bottomTeamName = bottomTable.find('td').next_sibling.next_sibling
-#topTeamName = topTable.find('td').next_sibling.next_sibling
 
 a= []
 b = []
@@ -23,46 +18,43 @@ spreadH = []
 ouA = []
 ouH = []
 
+def tables( top, bot):
+	while (bot != None):
+		if not a:
+			a.append(top)
+			b.append(bot)
+			c.append(top.find('td').next_sibling.next_sibling)
+		else :
+			top = bot.next_sibling.next_sibling
+			bot = top.next_sibling.next_sibling
+			a.append(top)
+			b.append(bot)
 
-end = True
-for i in range(9):
+def stacks():
+	for i in range(len(a) -1):
+		away.append(a[i].contents[3].text)
+		home.append(b[i].contents[3].text)
+		spreadA.append(a[i].contents[31].text)
+		spreadH.append(b[i].contents[31].text)
+		ouA.append(a[i].contents[33].text)
+		ouH.append(b[i].contents[33].text)
+
+def printTrends():
+	for i in range (len(away)):
+		print (away[i].replace('\r', '').replace('\n', '') + spreadA[i].replace('\r', '').replace('\n', '') + ouA[i].replace('\r', '').replace('\n', ''))
+		print (home[i].replace('\r', '').replace('\n', '') + spreadH[i].replace('\r', '').replace('\n', '') + ouH[i].replace('\r', '').replace('\n', ''))
 
 
-
-	if ( i < 1):
-		a.append(topTable)
-		b.append(bottomTable)
-		c.append(topTable.find('td').next_sibling.next_sibling)
-
-		
-	else :
-		
-		topTable = bottomTable.next_sibling.next_sibling
-		bottomTable = topTable.next_sibling.next_sibling
-		a.append(topTable)
-		b.append(bottomTable)
-		c.append(topTable.find('td').next_sibling.next_sibling)
-
-
-
-
-for i in range(len(a) -1):
-
-
-	away.append(a[i].contents[3].text)
-	home.append(b[i].contents[3].text)
-	spreadA.append(a[i].contents[31].text)
-	spreadH.append(b[i].contents[31].text)
-	ouA.append(a[i].contents[33].text)
-	ouH.append(b[i].contents[33].text)
+tables(topTable,bottomTable)
+stacks()
+printTrends()
 
 
 
 
 
-print (away[0])
-print (spreadA[0])
-print (ouA[0])
+
+
 
 
 
